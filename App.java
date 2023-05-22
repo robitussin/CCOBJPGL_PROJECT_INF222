af15bc54e78549ed76e9079186f6e089b8650e18
John Darrel Poquiz <111472863+johndarrel29@users.noreply.github.com>
2023-05-22 10:02:18 +0800

    Update App.java

--- App.java
+++ App.java
@@ -3,5 +3,7 @@ public class App {
     public static void main(String[] args) {
 
         System.out.println("Hello darrel!");
+        
+        //kahit ang isda nalunod nung di mo na ako miss eh
     }
 }
